53b7d7113a315bd43c32a591881032c7916cc202
a <a01375987@itesm.mx>
2019-02-10 21:48:56 -0600

    a

---
+++ desktop/src/itesm/mx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package itesm.mx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import itesm.mx.Principal;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Principal(), config);
+	}
+}
