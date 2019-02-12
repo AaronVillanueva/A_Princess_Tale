a0388696dcb36a6aad237e7f5a5d0e5e474dfa93
Jaime Lopez <a01374606@itesm.mx>
2019-02-12 14:07:12 -0600

    :)

--- core/src/itesm/mx/PantallaCargando.java
+++ core/src/itesm/mx/PantallaCargando.java
@@ -1,13 +1,18 @@
 package itesm.mx;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class PantallaCargando implements Screen{
 
     public static final int ANCHO = 1280;
     public static final int ALTO = 720;
-
     private final Principal principal;
+    private OrthographicCamera camara; //cámara
+    private Viewport vista;
+    private SpriteBatch batch;
     public PantallaCargando(Principal principal) {
         this.principal=principal;
     }
