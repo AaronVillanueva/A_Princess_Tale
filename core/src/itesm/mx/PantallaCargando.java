14e9d01dd771014f1e3c94c9894f2a16d5d329a5
Villanueva <a01375987@itesm.mx>
2019-02-12 13:55:59 -0600

    Test

---
+++ core/src/itesm/mx/PantallaCargando.java
@@ -0,0 +1,8 @@
+package itesm.mx;
+
+public class PantallaCargando {
+    private final Principal principal;
+    public PantallaCargando(principal juegoDemo) {
+        this.juego=principal;
+    }
+}
