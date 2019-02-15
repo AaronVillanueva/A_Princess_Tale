bf3cf294024ac5366bba2980f82d2ee073ff52b8
Villanueva <a01375987@itesm.mx>
2019-02-15 11:00:26 -0600

    PantallaAyuda agregada

---
+++ core/src/itesm/mx/PantallaAyuda.java
@@ -0,0 +1,40 @@
+package itesm.mx;
+
+import com.badlogic.gdx.Screen;
+
+public class PantallaAyuda extends Pantalla implements Screen {
+
+    @Override
+    public void show() {
+        inicializarShow();
+        crearFondo("Pantallas/PantallaAyuda.png");
+    }
+
+    @Override
+    public void render(float delta) {
+        batch.setProjectionMatrix(camara.combined);
+        batch.begin();
+        batch.draw(textFondo, 0, 0);
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+}
