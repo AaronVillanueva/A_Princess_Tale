8340e7d3e7765997c1a0d789fe22e263bc6d8947
Villanueva <a01375987@itesm.mx>
2019-03-19 13:39:56 -0600

    Bug en la tira de personaje. Revisar metodo de animación

--- core/src/itesm/mx/Elya.java
+++ core/src/itesm/mx/Elya.java
@@ -2,7 +2,7 @@ package itesm.mx;
 
 public class Elya extends Personaje {
     public Elya(){
-        cargarText("Personajes/ElyaRunning.png",23);
+        cargarText("Personajes/ElyaRunning.png",24);
 
 
     }
