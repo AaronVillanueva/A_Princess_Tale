4bc43573412115b2df2b7dc203ef4f20a8809b27
maferhernandezdiaz <ma.ferhdz1496@gmail.com>
2019-04-01 11:22:10 -0600

    Pantalla Configuración y Creditos ya puesta (Falta luego poner los botones bien)

--- core/src/itesm/mx/PantallaAjustes.java
+++ core/src/itesm/mx/PantallaAjustes.java
@@ -9,7 +9,7 @@ public class PantallaAjustes extends Pantalla implements Screen {
     @Override
     public void show() {
         inicializarShow();
-        crearFondo("Pantallas/PantallaAjustes.png");
+        crearFondo("Pantallas/Pantalla_Configuracion_CONBOTONES.png");
     }
 
     @Override
