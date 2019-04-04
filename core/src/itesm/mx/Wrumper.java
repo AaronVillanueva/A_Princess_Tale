e35726918fc110e97f5ff7e77ec1a6efbcf9c175
Jaime Lopez <a01374606@itesm.mx>
2019-04-04 17:29:39 -0600

    Tiempo en pantalla

--- core/src/itesm/mx/Wrumper.java
+++ core/src/itesm/mx/Wrumper.java
@@ -13,7 +13,7 @@ public class Wrumper extends Personaje {
         cargarText("Personajes/WrumperCorriendo.png",24,1);
         cargarText("Personajes/WrumperAtack.png",24,2);
         cargarText("Personajes/WrumperDead.png",1,3);
-        setPos(MathUtils.random(80, (int)Pantalla.ANCHO/2-80), Pantalla.ALTO/2-205);
+        setPos(MathUtils.random(80, (int)Pantalla.ANCHO-80), Pantalla.ALTO/2-205);
     }
     
     public void rastrearPrincesa(Elya elya){
