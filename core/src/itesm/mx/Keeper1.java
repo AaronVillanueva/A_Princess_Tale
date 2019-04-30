0f95aa0566ff3c0502ed92840ff19731d067d8db
jaimi <a01374696@itesm.mx>
2019-04-30 13:17:47 -0500

    Prueba Compu

--- core/src/itesm/mx/Keeper1.java
+++ core/src/itesm/mx/Keeper1.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.math.MathUtils;
 public class Keeper1 extends Wrumper {
 
     public Keeper1(){
+        System.out.println("Se crea un Keeper");
         vidas = 5;
         cargarText("Personajes/Kepper1Running.png", 24, 1);
         setPos(Pantalla.ANCHO+300, Pantalla.ALTO/2-205);
